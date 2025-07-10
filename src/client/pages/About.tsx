9c4bfb0ffa0db1d9082bb16aef74ebd5eb18a5fa
Vlashex <rmaynavine@gmail.com>
2025-07-10 16:05:37 +0300

    Тест CI/CD 3

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
       <div className="row">
         <div className="col">
           <h1>About</h1>
-          <p>Author: Жадан Владислав Ан</p>
+          <p>Author: Жадан Владислав Андре</p>
         </div>
       </div>
     </div>
